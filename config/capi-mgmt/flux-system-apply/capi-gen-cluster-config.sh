a74e37b1605bc853929ab559b1949ca11d4cb3ee
Piaras Hoban <phoban01@gmail.com>
2021-07-12 14:41:12 +0100

    update child cluster version

--- config/capi-mgmt/flux-system-apply/capi-gen-cluster-config.sh
+++ config/capi-mgmt/flux-system-apply/capi-gen-cluster-config.sh
@@ -1,7 +1,6 @@
-clusterctl \
-  generate cluster child \
+clusterctl generate cluster child \
   -n flux-system \
   --flavor development \
-  --kubernetes-version 1.19.1 \
+  --kubernetes-version 1.21.1 \
   --infrastructure docker:v0.4.0 \
   --worker-machine-count 1 \
