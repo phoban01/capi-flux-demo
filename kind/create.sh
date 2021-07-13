4a3b47ec45dad5ead11ccf4bf1ff27be466c001c
Piaras Hoban <phoban01@gmail.com>
2021-07-13 09:41:00 +0100

    clean up kind configs

--- kind/create.sh
+++ kind/create.sh
@@ -3,7 +3,6 @@ unset CD_PATH
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${SCRIPT_DIR}" || exit 1
 
-KIND_EXPERIMENTAL_DOCKER_NETWORK="bridge" \
-  kind create cluster \
+kind create cluster \
   --name "capi" \
   --config "./capi-mgmt.yaml"
